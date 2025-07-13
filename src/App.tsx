1095d6235fbf78baa372c5af971139d7b4a0d0ca
Felix <felikslyu@gmail.com>
2025-07-13 11:55:55 +1000

    add TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +20,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-3xl underline'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
